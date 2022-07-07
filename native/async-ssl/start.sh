76418db6733ceb5d07d06d9baa7448b569584e3f
Serg <sobolentsev@gmail.com>
2022-07-07 09:28:10 +0300

    add native client test

---
+++ native/async-ssl/start.sh
@@ -0,0 +1,5 @@
+./selenika-peer-server-test-client \
+--ssl_srt=/home/qaz/work/config/server.crt --ssl_key=/home/qaz/work/config/server.key \
+--server=localhost --port=8080 \
+--dbg_level=debug \
+--log=./selenika-peer-server-test-client.log
